1a240047f512b1793dc4edcc323205bda204f04f
jtward <jtward.is@gmail.com>
2025-03-17 11:31:13 +0100

    Add react router and styled components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes } from 'react-router';
+
 import './index.css';
-import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
