0a213b66f5c043dff3a55fbc174c94efd5b2edf4
jtward <jtward.is@gmail.com>
2025-03-18 10:49:48 +0100

    Add translation and query client providers to root

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import { BrowserRouter, Routes } from 'react-router';
 
 import './index.css';
 
+// translations
+import { LocalizationProvider } from './localization/LocalizationProvider.tsx';
+
+// query client
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-      </Routes>
-    </BrowserRouter>
+    <LocalizationProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </LocalizationProvider>
   </StrictMode>
 );
