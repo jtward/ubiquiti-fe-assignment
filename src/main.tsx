18b96944fceffff397d989451e44af06f84384c9
jtward <jtward.is@gmail.com>
2025-03-20 14:23:55 +0100

    Add page layout and routes to router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
 import './index.css';
 
@@ -14,14 +14,31 @@ import {
 } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
+// device pages use device search provider to retain state across navigation
+import { DeviceSearchProvider } from './pages/devices-list/contexts/DeviceSearchProvider.tsx';
+
+// layouts
+import { DefaultPageLayout } from './layouts/defaultPageLayout/defaultPageLayout.tsx';
+
+// pages
+import { DevicesListPage } from './pages/devices-list/DevicesListPage.tsx';
+import { DevicesDetailsPage } from './pages/device-details/DeviceDetailsPage.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocalizationProvider>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-          </Routes>
-        </BrowserRouter>
+        <DeviceSearchProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigate to="/devices" replace />} />
+              <Route element={<DefaultPageLayout />}>
+                <Route path="/devices" element={<DevicesListPage />} />
+                <Route path="/devices/:deviceId" element={<DevicesDetailsPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </DeviceSearchProvider>
       </QueryClientProvider>
     </LocalizationProvider>
   </StrictMode>
